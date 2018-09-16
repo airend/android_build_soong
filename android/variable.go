2676be3723086a5fc8ac879b42353a56c1e237a0
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-09-16 12:33:50 +0300

    variables: [TEMP] Add Cant_reallocate_omx_buffers
    Change-Id: I349aab8fc9e7bbef10c7d8e275eeb38133fbcd82
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- android/variable.go
+++ android/variable.go
@@ -100,6 +100,10 @@ type variableProperties struct {
 		Needs_platform_textrels struct {
 			Cppflags []string
 		}
+
+		Cant_reallocate_omx_buffers struct {
+			Cppflags []string
+		}
 	} `android:"arch_variant"`
 }
 
@@ -175,6 +179,8 @@ type productVariables struct {
 	DeviceKernelHeaders []string `json:",omitempty"`
 
 	Needs_platform_textrels *bool `json:",omitempty"`
+
+	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {
